e861e996afd0d70a73ac47f3d8eb0e798afa8722
MannyCasti1 <70231911+MannyCasti1@users.noreply.github.com>
2023-11-30 09:07:39 -0600

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+
+<template>
+<div> 
+<counter />  <!-- using templete to migrate my data from componets-->
+</div>
+</template>
+
+
+<script>
+import counter from './components/counter.vue' // coming from my componte.vue and all it data
+export default {
+  name: 'App',
+  components: {
+    counter
+  }
+}
+</script>
+
+<style>
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #1a9b09;
+  margin-top: 60px;
+}
+</style>
+
+
+
+
+
+
+    
+    
